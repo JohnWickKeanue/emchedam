86ab184739efe6485c9de148652af1f7982ee701
Romeo <92579700+JohnWickKeanue@users.noreply.github.com>
2022-04-02 11:16:49 +0530

    Update headers.py

--- headers.py
+++ headers.py
@@ -1,37 +1,19 @@
-
 import requests
 
-cookies = {
-    'country': 'IN',
-    'display-language': 'en',
-    'xaccesstoken': 'eyJ0eXAiOiJqd3QiLCJhbGciOiJIUzI1NiJ9.eyJwcm9kdWN0X2NvZGUiOiJ6ZWU1QDk3NSIsInBsYXRmb3JtX2NvZGUiOiJXZWJAJCF0Mzg3MTIiLCJpc3N1ZWRBdCI6IjIwMjItMDQtMDJUMDA6MzA6MDIrMDAwMCIsInR0bCI6ODY0MDB9.4RH1dilEOHsahhdu2HN6d2tnY8PN41OJUcJYJxjgu3I',
-    '_iidt': 'dS+vqnGyj/Ii2D/dGXqtlOUvY9nspR/2tPpNFejNYNqzaygaqWwpHyMM9tcGgpYYxZeQdqmut0JVSQ==',
-    '_vid_t': 'ogQZMIxW1ghOkGVu05qe+j9hftRG0nAAmAyLS5EcEgDGWIipaBIxNilqg4N1J1MLOtwtvK3DjF5PGA==',
-    'G_ENABLED_IDPS': 'google',
-    'state': 'TG',
-    'content-language': 'en,te',
-    'g_state': '{"i_t":1648962961136,"i_l":0}',
-    'user-type': 'premium',
-    'refreshToken': '677e4461e431ac87698fe96f023162c8b81a509a67be31de8d64ad92ae96480e',
-    'WZRK_S_RKW-4R7-785Z': '%7B%22p%22%3A1%7D',
-    'ssoToken': '883efedd7287347c72e6862dc43443f2',
-}
-
 headers = {
-    'authority': 'www.zee5.com',
     'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="99"',
-    'accept': '*/*',
+    'Referer': 'https://www.zee5.com/',
     'sec-ch-ua-mobile': '?1',
-    'save-data': 'on',
-    'user-agent': 'Mozilla/5.0 (Linux; Android 11; M2012K11AI) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.94 Mobile Safari/537.36',
+    'Save-Data': 'on',
     'sec-ch-ua-platform': '"Android"',
-    'sec-fetch-site': 'same-origin',
-    'sec-fetch-mode': 'no-cors',
-    'sec-fetch-dest': 'empty',
-    'referer': 'https://www.zee5.com/signin',
-    'accept-language': 'en-IN,en-GB;q=0.9,en-US;q=0.8,en;q=0.7',
-    # Requests sorts cookies= alphabetically
-    # 'cookie': 'country=IN; display-language=en; xaccesstoken=eyJ0eXAiOiJqd3QiLCJhbGciOiJIUzI1NiJ9.eyJwcm9kdWN0X2NvZGUiOiJ6ZWU1QDk3NSIsInBsYXRmb3JtX2NvZGUiOiJXZWJAJCF0Mzg3MTIiLCJpc3N1ZWRBdCI6IjIwMjItMDQtMDJUMDA6MzA6MDIrMDAwMCIsInR0bCI6ODY0MDB9.4RH1dilEOHsahhdu2HN6d2tnY8PN41OJUcJYJxjgu3I; _iidt=dS+vqnGyj/Ii2D/dGXqtlOUvY9nspR/2tPpNFejNYNqzaygaqWwpHyMM9tcGgpYYxZeQdqmut0JVSQ==; _vid_t=ogQZMIxW1ghOkGVu05qe+j9hftRG0nAAmAyLS5EcEgDGWIipaBIxNilqg4N1J1MLOtwtvK3DjF5PGA==; G_ENABLED_IDPS=google; state=TG; content-language=en,te; g_state={"i_t":1648962961136,"i_l":0}; user-type=premium; refreshToken=677e4461e431ac87698fe96f023162c8b81a509a67be31de8d64ad92ae96480e; WZRK_S_RKW-4R7-785Z=%7B%22p%22%3A1%7D; ssoToken=883efedd7287347c72e6862dc43443f2',
+    'Content-Type': 'application/x-www-form-urlencoded',
 }
 
-response = requests.get('https://www.zee5.com/', headers=headers, cookies=cookies)
+params = {
+    'ip': '1',
+    '_': '1648878262167',
+}
+
+data = 'data=eyIkc2V0IjogeyIkb3MiOiAiQW5kcm9pZCIsIiRicm93c2VyIjogIkNocm9tZSIsIiRicm93c2VyX3ZlcnNpb24iOiA5OSwiJGluaXRpYWxfcmVmZXJyZXIiOiAiaHR0cHM6Ly9yLnNlYXJjaC55YWhvby5jb20vX3lsdD1Bd3J4aFdpdjIwZGlNZ3NBalJXc25pTTU7X3lsdT1ZMjlzYndNRWNHOXpBekVFZG5ScFpBTUVjMlZqQTNOeS9SVj0yL1JFPTE2NDg5MDUyNjMvUk89MTAvUlU9aHR0cHMlM2ElMmYlMmZ3d3cuemVlNS5jb20lMmYvUks9Mi9SUz1kTGJJY3hUejZ5dE9McGZ5YTdfdjV1VWNiZWctIiwiJGluaXRpYWxfcmVmZXJyaW5nX2RvbWFpbiI6ICJyLnNlYXJjaC55YWhvby5jb20iLCJVbmlxdWUgSUQiOiAiOWEyNzVjMjYtNzFjNi00NmJiLWIzYTUtOTUyYThmNmE2MWRiIiwiUmVnaXN0cmluZyBDb3VudHJ5IjogIklOIiwiTmV3IEF1dG9wbGF5IFNldHRpbmciOiAidHJ1ZSIsIk5ldyBBcHAgTGFuZ3VhZ2UiOiAiZW4iLCJOZXcgQ29udGVudCBMYW5ndWFnZSI6IFsKICAgICJlbiIsCiAgICAidGUiCl0sIlVzZXIgVHlwZSI6ICJwcmVtaXVtIiwiTmV4dCBFeHBpcmluZyBQYWNrIjogWwogICAgIjAtMTEtMTg0Ml9QcmVtaXVtX1NWT0QiCl0sIk5leHQgUGFjayBFeHBpcnkgRGF0ZSI6ICIyMDIzLTAyLTE4VDIzOjU5OjU5WiIsIkZyZWUgVHJpYWwgRXhwaXJ5IERhdGUiOiAiTi9BIiwiUGFjayBEdXJhdGlvbiI6IDM2NSwiRnJlZSBUcmlhbCBQYWNrYWdlIjogIk4vQSIsIkdlbmRlciI6ICJNYWxlIiwiTGF0ZXN0IFN1YnNjcmlwdGlvbiBQYWNrIjogIjAtMTEtMTg0Ml9QcmVtaXVtX1NWT0QiLCJMYXRlc3QgU3Vic2NyaXB0aW9uIFBhY2sgRXhwaXJ5IjogIjIwMjMtMDItMThUMjM6NTk6NTlaIiwiUGFyZW50IENvbnRyb2wgU2V0dGluZyI6ICJOL0EiLCJBZ2UiOiAiTi9BIiwiUGFydG5lciBOYW1lIjogIlplZTUiLCJoYXNFZHVhdXJhYSI6IGZhbHNlLCJIYXNSZW50YWwiOiBmYWxzZX0sIiR0b2tlbiI6ICIyOGFkMWJkM2Y5ZmJjZGE3MzU2YzcwODM1YTBiNGQ5NiIsIiRkaXN0aW5jdF9pZCI6ICI5YTI3NWMyNi03MWM2LTQ2YmItYjNhNS05NTJhOGY2YTYxZGIiLCIkZGV2aWNlX2lkIjogIjE3ZmU4YjI0ZTU3NmVjLTAxNTZkMTIwYWMwZTg0LTI2NDI2YTEyLTUzYzMxLTE3ZmU4YjI0ZTU4N2U0IiwiJHVzZXJfaWQiOiAiOWEyNzVjMjYtNzFjNi00NmJiLWIzYTUtOTUyYThmNmE2MWRiIn0='
+
+response = requests.post('https://api-js.mixpanel.com/engage/', headers=headers, params=params, data=data)
